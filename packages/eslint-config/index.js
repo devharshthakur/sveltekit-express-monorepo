936a06a0faa94ff408a2505db81953d08fda7357
Harsh Thakur <harshprojects2002@outlook.com>
2026-07-05 22:57:58 +0530

    chore: add prettier formatting for root and packages

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import ts from 'typescript-eslint';
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import ts from "typescript-eslint";
 
 export { globals, ts };
 
@@ -12,15 +12,15 @@ export const baseConfig = defineConfig(
 	{
 		languageOptions: {
 			globals: {
-				...globals.node
-			}
+				...globals.node,
+			},
 		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
-	}
+			"no-undef": "off",
+		},
+	},
 );
 
 export const prettierConfig = prettier;
